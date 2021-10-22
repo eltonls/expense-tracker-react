a04799d1d4febd110f6160180c1e02513e64eef2
eltonnrs <eltonnrs25@gmail.com>
2021-10-22 14:06:57 -0300

    added expenses form and logic to add the expenses onto the list

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
+import Card from "../UI/Card";
+import "./Expenses.css";
+
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState();
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  return (
+    <Card className="expenses">
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
+
+      {props.items.map((item) => (
+        <ExpenseItem title={item.title} amount={item.amount} date={item.date} />
+      ))}
+    </Card>
+  );
+};
+
+export default Expenses;
